8f13b2bc945d2a23fcead232469ba7c337d44e3b
Bwangari <wanderobeth@gmail.com>
2020-07-01 15:44:20 +0300

    added jquery for development

--- js/master.js
+++ js/master.js
@@ -2,4 +2,11 @@ $(document).ready(function(){
     $("#desi").click(function() {
         $("#designp").toggle();
       });
+    $("#develop").click(function() {
+        $("#developp").toggle();
+      });
+      
  });
+ 
+
+ 
